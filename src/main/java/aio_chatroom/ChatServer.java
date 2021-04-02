214751f71caf81724636e29b48c5e5d5156a9455
lesleyping <741178981@qq.com>
2021-04-02 21:52:43 +0800

    tmp

---
+++ src/main/java/aio_chatroom/ChatServer.java
@@ -0,0 +1,75 @@
+package main.java.aio_chatroom;
+
+import java.io.Closeable;
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.channels.AsynchronousChannelGroup;
+import java.nio.channels.AsynchronousServerSocketChannel;
+import java.nio.channels.AsynchronousSocketChannel;
+import java.nio.channels.CompletionHandler;
+import java.nio.charset.Charset;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class ChatServer {
+    private static final String LOCALHOST = "localhost";
+    private static final int DEFAULT_PORT = 8888;
+    private static final String QUIT = "quit";
+    private static final int BUFFER = 1024;
+    private static final int THREADPOOL_SIZE = 8;
+
+    private AsynchronousChannelGroup channelGroup;
+    private AsynchronousServerSocketChannel serverChannel;
+    private Charset charset = Charset.forName("UTF-8");
+    private int port;
+
+    public ChatServer() {
+        this(DEFAULT_PORT);
+    }
+
+    public ChatServer(int port) {
+        this.port = port;
+    }
+
+    private boolean readyToQuit(String msg) {
+        return "quit".equals(msg);
+    }
+
+    private void close(Closeable closeable) {
+        if (closeable != null) {
+            try {
+                closeable.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void start() {
+        try {
+            ExecutorService executorService = Executors.newFixedThreadPool(THREADPOOL_SIZE);
+            channelGroup = AsynchronousChannelGroup.withThreadPool(executorService);
+            serverChannel = AsynchronousServerSocketChannel.open(channelGroup);
+            serverChannel.bind(new InetSocketAddress(LOCALHOST, port));
+            System.out.println("启动服务器，监听端口：" + port);
+
+            while (true) {
+                serverChannel.accept(null, new AcceptHandler());
+                System.in.read();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            close(serverChannel);
+        }
+
+    }
+
+    private Class AcceptHandler() implements CompletionHandler<AsynchronousSocketChannel, Object> {
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
