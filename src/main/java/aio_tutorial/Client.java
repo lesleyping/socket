1db680f35c52ca56ec10a8d23c43ac21dd9aefc7
lesleyping <741178981@qq.com>
2021-04-02 13:36:45 +0800

    aio tutorial

--- src/main/java/aio_tutorial/Client.java
+++ src/main/java/aio_tutorial/Client.java
@@ -1,8 +1,14 @@
 package main.java.aio_tutorial;
 
+import java.io.BufferedReader;
 import java.io.Closeable;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
 import java.nio.channels.AsynchronousSocketChannel;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
 
 public class Client {
     private String LOCALHOST = "localhost";
@@ -18,6 +24,44 @@ public class Client {
     }
 
     public void start() {
+        try {
+            clientChannel = AsynchronousSocketChannel.open();
+            Future<Void> future = clientChannel.connect(new InetSocketAddress(LOCALHOST, DEFAULT_PORT));
+            // 阻塞
+            future.get();
 
+            // 等待用户输入
+            BufferedReader consoleReader =
+                    new BufferedReader(new InputStreamReader(System.in));
+            while (true) {
+                String input = consoleReader.readLine();
+                // 发给服务器
+                byte[] inputBytes = input.getBytes();
+                ByteBuffer buffer = ByteBuffer.wrap(inputBytes);
+                Future<Integer> writeResult = clientChannel.write(buffer);
+                writeResult.get();
+
+                // 读服务器返回的消息
+                buffer.flip();
+                Future<Integer> readResult = clientChannel.read(buffer);
+                readResult.get();
+                String echo = new String(buffer.array());
+                buffer.clear();
+                System.out.println(echo);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        } finally {
+            close(clientChannel);
+        }
+    }
+
+    public static void main(String[] args) {
+        Client client = new Client();
+        client.start();
     }
 }
