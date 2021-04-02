403e6e80faeb2e380799680cd366ec08979f756d
lesleyping <741178981@qq.com>
2021-04-02 13:02:28 +0800

    aio

--- src/main/java/aio_tutorial/Client.java
+++ src/main/java/aio_tutorial/Client.java
@@ -1,4 +1,23 @@
 package main.java.aio_tutorial;
 
+import java.io.Closeable;
+import java.io.IOException;
+import java.nio.channels.AsynchronousSocketChannel;
+
 public class Client {
+    private String LOCALHOST = "localhost";
+    private int DEFAULT_PORT = 8888;
+    AsynchronousSocketChannel clientChannel;
+
+    private void close(Closeable closeable) {
+        try {
+            closeable.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void start() {
+
+    }
 }
